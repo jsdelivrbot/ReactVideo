3573054a37bfed09b68b786a7af6cb35f37694d2
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-10-24 11:20:17 +0200

    add base

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = "03ae646b7b054fc7c416fdc00803f967";
+const App = function (){
+    return <div>Salut les amis</div>
+}
+
+ReactDOM.render(<App/> , document.querySelector('.container'));
