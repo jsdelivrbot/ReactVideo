d5e512606f722320f68d2759f0251f09f20de29d
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-10-24 15:11:49 +0200

    add first query api axios

--- src/containers/app.js
+++ src/containers/app.js
@@ -3,7 +3,22 @@ import SearchBar from '../components/search-bar';
 import VideoList from './video-list';
 import axios from 'axios';
 
+const API_END_POINT = "https://api.themoviedb.org/3/";
+const POPULAR_MOVIES_URL = "discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images";
+const API_KEY = "api_key=03ae646b7b054fc7c416fdc00803f967";
+
 class App extends Component{
+    constructor(props){
+        super(props)
+        this.state ={movieList:{},currentMovie:{}};
+    }
+    componentWillMount(){
+        axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
+            this.setState({movieList:response.data.results.slice(0,6)});
+            this.setState({currentMovie:response.data.results[0]});
+            console.log("",this.state.movieList);
+        }.bind(this));
+    }
     render(){
         return (
             <div>
