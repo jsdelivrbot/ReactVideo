fab0b189e92ff6acd490c50f68d667711b9274a3
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-10-24 14:22:51 +0200

    affichage des movies avec .map()

---
+++ src/containers/video-list.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import VideoListItem from '../components/video-list-item';
+
+
+const VideoList = ()=> {
+    const movies = ["film1","film2","film3","film4","film5"];
+    return (
+        <div>
+            {
+                movies.map(movie => {
+                    return <VideoListItem movie={movie}/>
+                })
+            }
+            <ul>
+               
+              
+            </ul>
+        </div>
+  
+    )
+
+}
+export default VideoList;
