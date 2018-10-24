64026c95e53591c97f5fc7bfe32935039a26d5bd
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-10-24 12:05:46 +0200

    add state , et gestion d'evenements sur le input

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,8 +1,23 @@
 import React ,{Component} from 'react'
 
 class SearchBar extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {searchText :"",placeHolder:"Taper votre film..."}
+    }
     render(){
-        return <input/>
+        return (
+        <div>
+                <input onChange={this.handleChange.bind(this)} placeholder={this.state.placeHolder}/>
+                <p>{this.state.searchText}</p>
+        </div>
+        )
+    }
+    // Recupére la valeur de l'input et la place dans le state searchText
+    handleChange(event){
+        // utiliser bind pour faire refence au this
+      this.setState({searchText:event.target.value});
     }
    
 }
